f3245393314552bf09909ca4c27b252f4cbe3e2f
이춘호 <84308554+Paydaypermanently@users.noreply.github.com>
2022-11-06 02:25:34 +0900

    [CREATE] Link SSR

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -4,3 +4,36 @@ interface ISubwayForm {
   direction: string
   speed?: string
 }
+
+interface IGetRegularTrainCongestionParams {
+  line: 'one' | 'two' | 'three' | 'four' | 'five' | 'six' | 'seven' | 'eight' | 'nine'
+  stationName: string
+  way: '상행' | '하행'
+}
+
+interface IGetExpressTrainCongestionParams {
+  line: 'one' | 'two' | 'three' | 'four' | 'five' | 'six' | 'seven' | 'eight' | 'nine'
+  stationName: string
+  way: '상행' | '하행'
+  fest: 0 | 1
+}
+
+interface ITrainCongestion {
+  suc: boolean
+  data: {
+    order?: number
+    updnLine?: string
+    trainNmuber?: string
+    trainLineNm?: string
+    stationNm?: string
+    arrivaltime?: string
+    msg1?: string
+    msg2?: string
+    subWayStatus?: string
+    BokJobDo: {
+      suc: boolean
+      aver: number
+      line: number[]
+    }
+  }
+}
