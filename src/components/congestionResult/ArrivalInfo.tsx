ad6b050839cb8cfc231e4ef68f42f03399f4b273
이춘호 <84308554+Paydaypermanently@users.noreply.github.com>
2022-11-08 21:14:08 +0900

    [CREATE] Link Train Arrival Info

--- src/components/congestionResult/ArrivalInfo.tsx
+++ src/components/congestionResult/ArrivalInfo.tsx
@@ -7,11 +7,12 @@ interface IProps {
 function ArrivalInfo({congestion}: IProps) {
   return (
     <Wrapper>
+      <div className="title">열차 도착 정보</div>
       <div className="curStation">
         <span className="info">현재위치</span>
-        <span className="station">서울대입구</span>
+        <span className="station">{congestion.data?.msg2}</span>
       </div>
-      <div className="arrivalTime">2분 8초 뒤 도착</div>
+      <div className="arrivalTime">{congestion.data?.msg1} </div>
     </Wrapper>
   )
 }
@@ -21,6 +22,7 @@ export default ArrivalInfo
 const Wrapper = styled.div`
   width: 100%;
   height: 150px;
+  border-top: 1px solid lightgray;
   background: #ffffff 0% 0% no-repeat padding-box;
   opacity: 1;
   border-radius: 25px;
@@ -28,15 +30,21 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 100px;
+
+  .title {
+    margin-top: 20px;
+    font-weight: 500;
+  }
 
   .arrivalTime {
     font-weight: 500;
-    margin-top: 30px;
+    margin-top: 20px;
     color: #c53715;
   }
   .curStation {
     width: 80%;
-    margin-top: 25px;
+    margin-top: 10px;
     height: 40px;
     border-bottom: 1px solid lightgray;
     align-items: center;
